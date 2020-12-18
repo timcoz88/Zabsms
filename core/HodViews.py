e528b1493fdc7fce584e2c9b1961816416709452
safwanvk <safwanvalakundil@gmail.com>
2020-12-18 15:08:14 +0530

    :zap: Setup add staff

--- core/HodViews.py
+++ core/HodViews.py
@@ -1,4 +1,9 @@
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from core.models import CustomUser
 
 
 def admin_home(request):
@@ -7,3 +12,25 @@ def admin_home(request):
 
 def add_staff(request):
     return render(request, "hod_template/add_staff_template.html")
+
+
+def add_staff_save(request):
+    if request.method != "POST":
+        return HttpResponse("Method Not Allowed")
+    else:
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        address = request.POST.get("address")
+        try:
+            user = CustomUser.objects.create_user(username=username, password=password, email=email,
+                                                  last_name=last_name, first_name=first_name, user_type=2)
+            user.staffs.address = address
+            user.save()
+            messages.success(request, "Successfully Added Staff")
+            return HttpResponseRedirect(reverse("add_staff"))
+        except:
+            messages.error(request, "Failed to Add Staff")
+            return HttpResponseRedirect(reverse("add_staff"))
--- core/urls.py
+++ core/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('logout_user', views.logout_user, name="logout"),
     path('doLogin', views.doLogin, name="do_login"),
     path('admin_home', HodViews.admin_home, name="admin_home"),
-    path('add_staff', HodViews.add_staff, name="add_staff")
+    path('add_staff', HodViews.add_staff, name="add_staff"),
+    path('add_staff_save', HodViews.add_staff_save, name="add_staff_save"),
 ]
