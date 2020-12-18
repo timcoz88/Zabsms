3ee1cc0f44c23653a626b89e8c1e237eac7a9c45
safwanvk <safwanvalakundil@gmail.com>
2020-12-18 10:35:32 +0530

    :zap: Setup config mysql

--- sms/settings.py
+++ sms/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sms',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
