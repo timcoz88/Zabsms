09cd7ac01e3ffcb69236ee51a03c5d721449a85f
safwanvk <safwanvalakundil@gmail.com>
2020-12-18 12:40:36 +0530

    :zap: Setup email backend for login

--- sms/settings.py
+++ sms/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = "core.CustomUser"
+AUTHENTICATION_BACKENDS = ['core.EmailBackEnd.EmailBackEnd']
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
