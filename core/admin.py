198f1046b0b17bddfc34d8f441c6178b9df94021
safwanvk <safwanvalakundil@gmail.com>
2020-12-18 12:27:04 +0530

    :zap: Setup admin and create user profile and save

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from core.models import CustomUser
+
+
+class UserModel(UserAdmin):
+    pass
+
+
+admin.site.register(CustomUser, UserModel)
--- core/models.py
+++ core/models.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
 class CustomUser(AbstractUser):
     user_type_data = ((1, "HOD"), (2, "Staff"), (3, "Student"))
     user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
@@ -134,3 +137,26 @@ class NotificationStaffs(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        if instance.user_type == 1:
+            AdminHOD.objects.create(admin=instance)
+        if instance.user_type == 2:
+            Staffs.objects.create(admin=instance, address="")
+        if instance.user_type == 3:
+            Students.objects.create(admin=instance, course_id=Courses.objects.get(id=1),
+                                    session_start_year="2020-01-01", session_end_year="2021-01-01", address="",
+                                    profile_pic="", gender="")
+
+
+@receiver(post_save, sender=CustomUser)
+def save_user_profile(sender, instance, **kwargs):
+    if instance.user_type == 1:
+        instance.adminhod.save()
+    if instance.user_type == 2:
+        instance.staffs.save()
+    if instance.user_type == 3:
+        instance.students.save()
