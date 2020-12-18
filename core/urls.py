214c20fde85f9784f01693221db0acae5fc5f3b6
safwanvk <safwanvalakundil@gmail.com>
2020-12-18 14:43:55 +0530

    :art: Setup home page template

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from core import views
+from core import views, HodViews
 
 urlpatterns = [
     path('demo', views.showDemoPage, name="demo_page"),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('get_user_details', views.GetUserDetails),
     path('logout_user', views.logout_user, name="logout"),
     path('doLogin', views.doLogin, name="do_login"),
+    path('admin_home', HodViews.admin_home, name="admin_home")
 ]
