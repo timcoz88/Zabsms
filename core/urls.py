15c4dbff0eb5225394f4420df9e561e19a604591
safwanvk <safwanvalakundil@gmail.com>
2020-12-18 12:54:22 +0530

    :zap: Setup login and logout

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,8 @@ from core import views
 
 urlpatterns = [
     path('demo', views.showDemoPage, name="demo_page"),
-    path('', views.showLoginPage, name="login_page")
+    path('', views.showLoginPage, name="login_page"),
+    path('get_user_details', views.GetUserDetails),
+    path('logout_user', views.logout_user, name="logout"),
+    path('doLogin', views.doLogin, name="do_login"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,49 @@
+from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
+from django.urls import reverse
+
+from core.EmailBackEnd import EmailBackEnd
+
+
 def showDemoPage(request):
     return render(request, "demo.html")
 
 
 def showLoginPage(request):
     return render(request, "login.html")
+
+
+def doLogin(request):
+    if request.method != "POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        user = EmailBackEnd.authenticate(request, username=request.POST.get("email"),
+                                         password=request.POST.get("password"))
+        if user is not None:
+            login(request, user)
+            if user.user_type == "1":
+                return HttpResponseRedirect('/admin_home')
+            elif user.user_type == "2":
+                return HttpResponseRedirect(reverse("staff_home"))
+            else:
+                return HttpResponseRedirect(reverse("student_home"))
+        else:
+            messages.error(request, "Invalid Login Details")
+            return HttpResponseRedirect("/")
+
+
+def GetUserDetails(request):
+    if request.user is not None:
+        return HttpResponse("User : " + request.user.email + " usertype : " + str(request.user.user_type))
+    else:
+        return HttpResponse("Please Login First")
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect("/")
