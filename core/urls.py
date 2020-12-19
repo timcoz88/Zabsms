c35677b92049be1661e0019d298e7245e546e0f4
safwanvk <safwanvalakundil@gmail.com>
2020-12-19 15:55:14 +0530

    :zap: Setup LoginCheckMiddleWare for page restriction

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from core import views, HodViews, StaffViews, StudentViews
 
 urlpatterns = [
     path('demo', views.showDemoPage, name="demo_page"),
-    path('', views.showLoginPage, name="login_page"),
+    path('', views.showLoginPage, name="show_login"),
     path('get_user_details', views.GetUserDetails),
     path('logout_user', views.logout_user, name="logout"),
     path('doLogin', views.doLogin, name="do_login"),
