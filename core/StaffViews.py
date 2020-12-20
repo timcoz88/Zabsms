c777fa342a6485d50b8f37a6fb9977938a8fee63
safwanvk <safwanvalakundil@gmail.com>
2020-12-20 14:05:21 +0530

    :zap: Setup save attendance

--- core/StaffViews.py
+++ core/StaffViews.py
@@ -1,9 +1,9 @@
 import json
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
-from core.models import Subjects, SessionYearModel, Students
+from core.models import Subjects, SessionYearModel, Students, Attendance, AttendanceReport
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -32,3 +32,31 @@ def get_students(request):
         data_small = {"id": student.admin.id, "name": student.admin.first_name + " " + student.admin.last_name}
         list_data.append(data_small)
     return JsonResponse(json.dumps(list_data), content_type="application/json", safe=False)
+
+
+@csrf_exempt
+def save_attendance_data(request):
+    student_ids = request.POST.get("student_ids")
+    subject_id = request.POST.get("subject_id")
+    attendance_date = request.POST.get("attendance_date")
+    session_year_id = request.POST.get("session_year_id")
+
+    subject_model = Subjects.objects.get(id=subject_id)
+    session_model = SessionYearModel.object.get(id=session_year_id)
+    json_sstudent = json.loads(student_ids)
+    # print(data[0]['id'])
+
+    try:
+        attendance = Attendance(subject_id=subject_model, attendance_date=attendance_date,
+                                session_year_id=session_model)
+        attendance.save()
+
+        for stud in json_sstudent:
+            student = Students.objects.get(admin=stud['id'])
+            attendance_report = AttendanceReport(student_id=student, attendance_id=attendance, status=stud['status'])
+            attendance_report.save()
+        return HttpResponse("OK")
+    except:
+        return HttpResponse("ERR")
+
+
