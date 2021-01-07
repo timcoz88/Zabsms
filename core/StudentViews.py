43b76f2fb4f625907487977114c3dbbfb807f387
safwanvk <safwanvalakundil@gmail.com>
2021-01-07 17:34:08 +0530

    :art: Setup student apply leave layout

--- core/StudentViews.py
+++ core/StudentViews.py
@@ -1,6 +1,10 @@
 import datetime
 
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 from core.models import Students, Subjects, CustomUser, Attendance, AttendanceReport, LeaveReportStudent, FeedBackStudent
 
