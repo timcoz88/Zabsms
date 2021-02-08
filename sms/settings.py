f5319875fc1e493599615cf2daaf022d51a3cf21
safwanvk <safwanvalakundil@gmail.com>
2021-02-08 20:38:56 +0530

    :deploy: Deploy to heroku

--- sms/settings.py
+++ sms/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -138,3 +139,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
+import django_heroku
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
